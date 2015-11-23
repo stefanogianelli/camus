d3b38ae38ee2962550c2e857812ee3d7f6cd8692
stefanogianelli <stefano.gianelli@gmail.com>
2015-11-23 23:24:24 +0100

    Implemented similarities algorithm in ResponseAggregator component

--- components/responseAggregator.js
+++ components/responseAggregator.js
@@ -1,15 +1,74 @@
 var _ = require('lodash');
+var similarityUtils = require('clj-fuzzy');
 var Promise = require('bluebird');
+
+//regular expression used for delete all the special symbols from the string for the comparison
+var regexp = '/[^\w\s]/gi, \'\'';
+//this threshold is used for identify a pair of items as similar. Less value are better (0.1 means 90% similarity)
+var threshold = 0.1;
 
 var responseAggregator = function () { };
 
+/**
+ * Remove duplicate items from the responses lists and append the information for retrieve information by the support services
+ * @param responses The list of responses
+ * @param supportServices The list of support service descriptions
+ * @returns {bluebird|exports|module.exports} The aggregated and cleaned response
+ */
 responseAggregator.prototype.prepareResponse = function (responses, supportServices) {
     return new Promise (function (resolve, reject) {
         var response = {};
-        response['data'] = _.flatten(responses);
-        response['support'] = [];
+        response['data'] = _.flatten(responseAggregator.prototype.findSimilarities(responses));
+        response['support'] = supportServices;
         resolve(response);
     });
+};
+
+/**
+ * Check if different arrays contains the same item.
+ * When a similar pair is found, the two items are merged and the second instance is removed.
+ * @param responses The array that contains all the responses
+ * @returns {*} The responses list without duplicate items
+ */
+responseAggregator.prototype.findSimilarities = function findSimilarities (responses) {
+    if (!_.isUndefined(responses) && !_.isEmpty(responses)) {
+        for(var i = 0; i < responses.length - 1; i++) {
+            for (var j = i + 1; j < responses.length; j++) {
+                _.forEach(responses[i], function(obj1, index) {
+                    _.forEach(responses[j], function (obj2) {
+                        if (responseAggregator.prototype.calculateObjectSimilarity(obj1, obj2)) {
+                            responses[i][index] = _.assign(obj2, obj1);
+                            _.remove(responses[j], obj2);
+                        }
+                    })
+                });
+            }
+        }
+        return responses;
+    } else {
+        console.log('Responses array is empty');
+    }
+};
+
+/**
+ * Calculate the similarity of two objects.
+ * It takes the intersection of common attributes of the two objects. Only string values are take in consideration.
+ * It uses the {@link https://en.wikipedia.org/wiki/Jaccard_index|Jaccard Distance} to calculate the strings similarity.
+ * To be similar, the calculated index must be less or equal than a predefined threshold.
+ * @param obj1 The first object
+ * @param obj2 The second object
+ * @returns {boolean} True if the two objects are similar, false otherwise
+ */
+responseAggregator.prototype.calculateObjectSimilarity = function calculateObjectSimilarity (obj1, obj2) {
+    var similarities = [];
+    var intersect = _.intersection(_.keysIn(obj1), _.keysIn(obj2));
+    _.forEach(intersect, function(i) {
+        if (_.isString(obj1[i]) && _.isString(obj2[i])) {
+            similarities.push(similarityUtils.metrics.jaccard(obj1[i].replace(regexp), obj2[i].replace(regexp)));
+        }
+    });
+    var similarity = _.sum(similarities)/similarities.length;
+    return similarity <= threshold;
 };
 
 module.exports = new responseAggregator();
--- test/responseAggregatorTest.js
+++ test/responseAggregatorTest.js
@@ -6,199 +6,142 @@ describe('Component: ResponseAggregator', function () {
     describe('#prepareResponse()', function () {
         it('check correct response aggregation', function () {
             return responseAggregator
-                .prepareResponse(testResponses, null)
+                .prepareResponse(testSuccessfulMerging, supportResponse)
                 .then(function (response) {
                     assert.notEqual(response, null);
-                    //console.log(response);
-                })
+                    assert.equal(response.data.length, 3);
+                    assert.equal(response.support.length, 3);
+Im                })
+        });
+    });
+
+    describe('#findSimilarities()', function () {
+        it('check if similar items are correctly merged', function () {
+            var response = responseAggregator.findSimilarities(testSuccessfulMerging);
+            assert.equal(response[0][0].nome, 'Bottega Ghiotta Gourmet');
+            assert.equal(response[0][0].tipologia, 'Ristorante Italiano, Pizzeria');
+            assert.equal(response[0][0].rating, '5/5');
+            assert.equal(response[0][0].recensioni, 24);
+            assert.equal(response[0][0].indirizzo, 'Corso Sempione, 5, 20154 Milano, Italia');
+            assert.equal(response[0][0].telefono, '+39 02 3956 6018');
+            assert.equal(response[0][0].website, 'http://www.bottegaghiotta.it/');
+            assert.equal(response[0][1].nome, 'Savana Ristorante Eritreo');
+            assert.equal(response[0][1].rating, '5/5');
+            assert.equal(response[0][1].recensioni, 61);
+            assert.equal(response[0][1].indirizzo, 'Via Luigi Canonica 45, 20154 Milano, Italia');
+            assert.equal(response[0][1].telefono, '393664073136');
+            assert.equal(response[0][1].website, 'http://www.ristoranteritreosavana.com/');
+            assert.equal(response[1][0].nome, 'Nerino Dieci Trattoria');
+            assert.equal(response[1][0].rating, '4.5/5');
+            assert.equal(response[1][0].recensioni, 2502);
+            assert.equal(response[1][0].indirizzo, 'Via Nerino 10, 20123 Milano, Italia');
+            assert.equal(response[1][0].telefono, '+39 02 3983 1019');
+            assert.equal(response[1][0].website, 'http://www.nerinodieci.it/');
+            assert.equal(response[1][0].tipologia, 'Ristorante Italiano');
+        });
+    });
+
+    describe('#calculateObjectSimilarity()', function () {
+        it('check if similar objects are identified', function () {
+            assert.equal(responseAggregator.calculateObjectSimilarity(baseObject, similarObject), true);
+        });
+        it('check if different objects are identified', function () {
+            assert.equal(responseAggregator.calculateObjectSimilarity(baseObject, differentObject), false);
         });
     });
 
 });
 
-var testResponses = [
+//Base object for similarity testing
+var baseObject = {
+    title: 'Spectre',
+    director: 'Sam Mendes',
+    writtenBy: 'John Logan, Neal Purvis, Robert Wade, Jez Butterworth',
+    starring: 'Daniel Craig, Christoph Waltz, Lea Seydoux, Ben Whishaw'
+};
+
+//Example of an object similar to the base one
+var similarObject = {
+    title: 'Spectre',
+    director: 'Mendes J. Sam',
+    writtenBy: 'John Logan, Robert Wade, Jez Butterworth, Neal Purvis',
+    starring: 'Daniel Craig as James Bond, Christoph Waltz as Franz Oberhauser, Lea Seydoux as Dr. Madeleine Swann'
+};
+
+//Example of an object different from the base one
+var differentObject = {
+    title: 'Burnt',
+    director: 'John Wells',
+    writtenBy: 'Steven Knight',
+    starring: 'Bradley Cooper as Adam Jones, Sienna Miller as Helene, Omar Sy as Michel'
+};
+
+//response used to test if two similar items are merged correctly
+var testSuccessfulMerging = [
     [
         {
-            title: 'Bouley',
-            address: '163 Duane St, New York, NY 10013, Stati Uniti',
-            latitude: 40.7169501,
-            longitude: -74.0089346
+            nome: 'Bottega Ghiotta Gourmet',
+            rating: '5/5',
+            recensioni: 24,
+            indirizzo: 'Corso Sempione, 5, 20154 Milano, Italia',
+            telefono: '+39 02 3956 6018',
+            website: 'http://www.bottegaghiotta.it/'
         },
         {
-            title: 'Eleven Madison Park',
-            address: '11 Madison Ave, New York, NY 10010, Stati Uniti',
-            latitude: 40.741509,
-            longitude: -73.9864808
-        },
-        {
-            title: 'Gramercy Tavern',
-            address: '42 E 20th St, New York, NY 10003, Stati Uniti',
-            latitude: 40.7384596,
-            longitude: -73.9885037
-        },
-        {
-            title: 'Le Bernardin',
-            address: '155 W 51st St, New York, NY 10019, Stati Uniti',
-            latitude: 40.7616941,
-            longitude: -73.98188309999999
-        },
-        {
-            title: 'Daniel',
-            address: '60 E 65th St, New York, NY 10065, Stati Uniti',
-            latitude: 40.7667384,
-            longitude: -73.9676095
-        },
-        {
-            title: 'Jean-Georges',
-            address: '1 Central Park West, New York, NY 10023, Stati Uniti',
-            latitude: 40.769084,
-            longitude: -73.981431
-        },
-        {
-            title: 'Per Se',
-            address: '4, 10 Columbus Cir, New York, NY 10019, Stati Uniti',
-            latitude: 40.7683519,
-            longitude: -73.98319839999999
-        },
-        {
-            title: 'The Modern',
-            address: '9 W 53rd St, New York, NY 10019, Stati Uniti',
-            latitude: 40.7610805,
-            longitude: -73.9767531
-        },
-        {
-            title: 'Marea',
-            address: '240 Central Park S, New York, NY 10019, Stati Uniti',
-            latitude: 40.7674054,
-            longitude: -73.98105679999999
-        },
-        {
-            title: 'Aureole',
-            address: '135 W 42nd St, New York, NY 10036, Stati Uniti',
-            latitude: 40.7556325,
-            longitude: -73.9852575
-        },
-        {
-            title: 'Gotham Bar & Grill',
-            address: '12 E 12th St, New York, NY 10003, Stati Uniti',
-            latitude: 40.7340835,
-            longitude: -73.9938031
-        },
-        {
-            title: 'The Four Seasons',
-            address: '99 E 52nd St, New York, NY 10022, Stati Uniti',
-            latitude: 40.7584364,
-            longitude: -73.9721783
-        },
-        {
-            title: 'Blue Hill',
-            address: '75 Washington Pl, New York, NY 10011, Stati Uniti',
-            latitude: 40.73202819999999,
-            longitude: -73.9996946
-        },
-        {
-            title: 'Annisa',
-            address: '13 Barrow St, New York, NY 10014, Stati Uniti',
-            latitude: 40.7325755,
-            longitude: -74.00244289999999
-        },
-        {
-            title: 'La Grenouille',
-            address: '3 E 52nd St, New York, NY 10022, Stati Uniti',
-            latitude: 40.75965900000001,
-            longitude: -73.975597
-        },
-        {
-            title: 'Scarpetta',
-            address: '355 W 14th St, New York, NY 10014, Stati Uniti',
-            latitude: 40.74093789999999,
-            longitude: -74.00481409999999
-        },
-        {
-            title: 'Maialino',
-            address: '2 Lexington Ave, New York, NY 10010, Stati Uniti',
-            latitude: 40.7385242,
-            longitude: -73.9857366
-        },
-        {
-            title: 'Le Cirque',
-            address: '151 E 58th St, New York, NY 10022, Stati Uniti',
-            latitude: 40.76131150000001,
-            longitude: -73.9675393
-        },
-        {
-            title: 'ABC Kitchen',
-            address: '35 E 18th St, New York, NY 10003, Stati Uniti',
-            latitude: 40.73775819999999,
-            longitude: -73.989625
-        },
-        {
-            title: 'Nobu Fifty Seven',
-            address: 'west 10019, 40 W 57th St, New York, NY 10019, Stati Uniti',
-            latitude: 40.7635397,
-            longitude: -73.9764228
+            nome: 'Savana Ristorante Eritreo',
+            rating: '5/5',
+            recensioni: 61,
+            indirizzo: 'Via Luigi Canonica 45, 20154 Milano, Italia',
+            telefono: '393664073136',
+            website: 'http://www.ristoranteritreosavana.com/'
         }
     ],
     [
         {
-            title: 'International Restaurant & Foodservice Show-New York',
-            address: '655 West 34th Street',
-            latitude: '40.757644',
-            longitude: '-74.002962'
+            nome: 'Bottega Ghiotta Gourmet',
+            tipologia: 'Ristorante Italiano, Pizzeria',
+            rating: '5/5',
+            recensioni: 21,
+            indirizzo: 'Corso Sempione 5, Milano',
+            telefono: '02 3956 6018',
+            website: 'http://www.bottegaghiotta.it/'
         },
         {
-            title: 'The Evolution of The New York City Restaurant',
-            address: '24 Irving Ave',
-            latitude: '40.7041',
-            longitude: '-73.9181'
-        },
+            nome: 'Nerino Dieci Trattoria',
+            rating: '4.5/5',
+            recensioni: 2502,
+            indirizzo: 'Via Nerino 10, 20123 Milano, Italia',
+            telefono: '+39 02 3983 1019',
+            website: 'http://www.nerinodieci.it/'
+        }
+    ],
+    [
         {
-            title: 'The View Restaurant 5-Course Wine Dinner',
-            address: '1535 Broadway',
-            latitude: '40.7583428',
-            longitude: '-73.9855814'
-        },
-        {
-            title: 'Business Planning for Restaurants',
-            address: '110 William St., 4th Floor',
-            latitude: '40.7095',
-            longitude: '-74.0012'
-        },
-        {
-            title: 'Restaurant Management Bootcamp 2.0, Lower Manhattan 11/9/15',
-            address: '110 William St., 4th Floor',
-            latitude: '40.7095',
-            longitude: '-74.0012'
-        },
-        {
-            title: 'Restaurant Management Bootcamp 2.0: Legal Considerations When Opening a Bar/Restaurant, (1 Session), Brooklyn 12/2/15',
-            address: '9 Bond Street, 5th Floor',
-            latitude: '40.6942',
-            longitude: '-73.9906'
-        },
-        {
-            title: 'New Year\'s Eve: Dueling Pianos\' "Shake, Rattle & Roll"',
-            address: '22 Warren Street',
-            latitude: '40.7142589',
-            longitude: '-74.0079081'
-        },
-        {
-            title: 'Dinner with Terry & George in New York',
-            address: '306 West 51st Street',
-            latitude: '40.7142',
-            longitude: '-74.0064'
-        },
-        {
-            title: 'New York City Jumpstart Alumni Focus Group',
-            address: '505 8th Avenue , Suite 1100',
-            latitude: '40.7142',
-            longitude: '-74.0064'
-        },
-        {
-            title: 'Third Annual Santa’s Winter Workshop at the Conrad New York',
-            address: '102 North End Avenue',
-            latitude: '40.7146608',
-            longitude: '-74.0151044'
+            nome: 'Nerino Dieci Trattoria',
+            rating: '4.5/5',
+            recensioni: 502,
+            indirizzo: 'Via Nerino 10, 20123 Milano, Italia',
+            telefono: '+39 02 3983 1019',
+            website: 'http://www.nerinodieci.it/',
+            tipologia: 'Ristorante Italiano'
         }
     ]
+];
+
+//test response that came from SupportServiceSelection component
+var supportResponse = [
+    {
+        name: 'Wikipedia',
+        url: 'https://en.wikipedia.org/w/api.php?action=query&titles={search_key}&prop=revisions&rvprop=content&format=json'
+    },
+    {
+        category: 'Transport',
+        service: 'ATM',
+        url: 'http://api.atm-mi.it/searchAddress'
+    },
+    {
+        category: 'Transport',
+        service: 'Trenord',
+        url: NaN
+    }
 ];
