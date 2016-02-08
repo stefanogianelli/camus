494e501cfa0b586351a0191270a5853b359b1e05
stefanogianelli <stefano.gianelli@gmail.com>
2016-02-08 09:42:48 +0100

    Added newrelic monitoring support

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 require('babel-register');
 require('babel-polyfill');
+if (process.env.NEW_RELIC_LICENSE_KEY !== undefined && process.env.NEW_RELIC_APP_NAME !== undefined) {
+    require('newrelic');
+}
 require('./server');
